8312605d647ac4f78ef34511bad46d2f67a49380
aniketbadadhe49-devops <aniketbadadhe49@gmail.com>
2025-11-01 16:50:42 +0530

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -10,23 +10,6 @@ module "vpc" {
 
 ###======================================================================
 
-# Security Group for EKS Cluster
-resource "aws_security_group" "eks_sg" {
-  name   = "eks-sg"
-  vpc_id = module.vpc.vpc_id
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   
